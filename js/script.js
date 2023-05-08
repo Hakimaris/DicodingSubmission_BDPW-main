dd7b012ad9e6783973e20b2174b98e0ae97e20db
Hakim <Hakimfatih30@gmail.com>
2023-05-08 15:24:30 +0700

    actual push

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById('toggleButton');
-const list = document.getElementById('navlist');
-
-toggle.addEventListener('click', () => {
-  list.classList.toggle('active');
-})
